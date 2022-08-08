efaa612316e1d29734fc7208cc8d1ab4a4d576a5
kseniya <grickovaKS@gmail.com>
2022-08-08 21:57:16 +0200

    change moment to dayjs

--- src/components/TimeStamp/TimeStamp.tsx
+++ src/components/TimeStamp/TimeStamp.tsx
@@ -1,16 +1,16 @@
 import React, { useState } from "react";
 import '../TimeStamp/TimeStamp.css';
-import moment from 'moment';
+import * as dayjs from 'dayjs';
 
 export function TimeStamp(props) {
     const format = props.dateFormat.replace('hh', 'HH');
-    const defaultValue = moment(props.defaultValue || new Date()).format(format);
+    const defaultValue = dayjs(props.defaultValue || new Date()).format(format);
     const [fieldValue, setFieldValue] = useState(defaultValue);
-
+    // const separators = format.test(/[a-z]/gi, '');
     function checkDate(fieldValue: string) {
-        var moment = require('moment');
-        var date = (!moment(fieldValue, format, true).isValid()) ? (fieldValue == '' ? null : undefined) : moment(fieldValue).format(format)
-        moment().toDate();
+        var date = !dayjs(fieldValue, format, true).isValid() ? (fieldValue === 
+            '' ? null : (/^(?![0-9]+$).*$/.test(fieldValue) ? setShowMsg({ visibility: "visible"}) : undefined)) : dayjs(fieldValue).format(format)
+        dayjs().toDate();
         console.log(date)
         return props.valueChanged(date, fieldValue);
     }
@@ -19,6 +19,7 @@ export function TimeStamp(props) {
 
     return (
         <div className="time-stamp">
+            <div>date format {props.dateFormat}</div>
             <label>
                 Enter the date:
                 <input type="text"
@@ -28,17 +29,15 @@ export function TimeStamp(props) {
                     onKeyPress={(event) => event.key === 'Enter' ? checkDate(fieldValue) : ""}
                     onChange={event => {
                         setFieldValue(event.target.value);
-                        var moment2 = require('moment');
-                        console.log(moment2(fieldValue, format, true).isValid())
-                        const param = moment2(fieldValue, format, true).isValid() === true ? "visible": "hidden"
+                        console.log(dayjs(fieldValue, format, true).isValid())
+                        const param = dayjs(fieldValue, format, true).isValid() ? "hidden" : "visible"
                         setShowMsg({ visibility: param})
-                        console.log(showMsg)
+                        console.log("showMsg", showMsg)
                     }}
                     placeholder={props.dateFormat}
                     value={fieldValue} />
                     <div className="error-msg" style={showMsg}>Wrong date format!</div>
             </label>
-            
         </div>
     );
 }
